6f1da41ae1a6545eb30794d47d80a6570463c4c0
kiieer <christliebkiersten@gmail.com>
2022-03-28 23:26:13 -0400

    3/28/2022
    defeated all of the mandatory test cases + the test suite

--- PCTCBank/target/classes/data.sql
+++ PCTCBank/target/classes/data.sql
@@ -1,12 +1,12 @@
-create table client(
+create table client (
 id serial primary key, full_name varchar(30));
 
 create table account(id serial primary key, account_number varchar, account_balance numeric, 
-client_id integer, foreign key (client_id) references client(id));
+client_id integer, constraint fk_client foreign key (client_id) references client(id) on delete cascade);
 
-insert into client(full_name) values ('Kiersten Christlieb');
-insert into client(full_name) values ('Jared Smith');
-insert into client(full_name) values ('Brandon Clark');
+insert into client(full_name) values ('Spongebob Squarepants');
+insert into client(full_name) values ('Patrick Star');
+insert into client(full_name) values ('Squidward Tentacles');
 
 
 insert into account(account_number, account_balance, client_id) values ('A01', 300, 1);
@@ -15,10 +15,14 @@ insert into account(account_number, account_balance, client_id) values ('A03', 5
 insert into account(account_number, account_balance, client_id) values ('B01', 7000,1);
 
 -- create three more of each client and accounts 
-insert into client(full_name) values ('Jasmine Buchee');
-insert into client(full_name) values ('Jerome Halla');
-insert into client(full_name) values ('Alexia Hilmont');
+insert into client(full_name) values ('Pearl Krabs');
+insert into client(full_name) values ('Sandy Cheeks');
+insert into client(full_name) values ('Bubble Bass');
 insert into account(account_number, account_balance, client_id) values ('A04', 300, 4);
 insert into account(account_number, account_balance, client_id) values ('A05', 400, 5);
 insert into account(account_number, account_balance, client_id) values ('A06', 500, 6);
 insert into account(account_number, account_balance, client_id) values ('B04', 7000, 4);
+
+
+drop table client;
+drop table account;
